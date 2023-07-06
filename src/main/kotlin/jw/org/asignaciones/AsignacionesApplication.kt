6d45b13573aeaa59d4b7cf08d7664a72ed3fc930
Norberto Sánchez <norber_san@hotmail.com>
2023-07-06 19:36:18 +0200

    initial commit

---
+++ src/main/kotlin/jw/org/asignaciones/AsignacionesApplication.kt
@@ -0,0 +1,11 @@
+package jw.org.asignaciones
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsignacionesApplication
+
+fun main(args: Array<String>) {
+    runApplication<AsignacionesApplication>(*args)
+}
---
+++ src/test/kotlin/jw/org/asignaciones/AsignacionesApplicationTests.kt
@@ -0,0 +1,13 @@
+package jw.org.asignaciones
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsignacionesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
